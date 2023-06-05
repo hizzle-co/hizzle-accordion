9c89aca8e9acd3a9fa2951689539076da6061cf2
picocodes <picocodes@gmail.com>
2023-06-05 16:38:33 +0300

    rebuild

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '383eb32f366821238303');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e0e7f7de6fd9a2aee965');
