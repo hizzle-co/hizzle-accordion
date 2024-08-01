449021c188129a8ab0039403f659f8643ff9c3c2
picocodes <picocodes@users.noreply.github.com>
2024-08-01 11:02:12 +0000

    Build and increment version [skip ci]

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e0e7f7de6fd9a2aee965');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'beb68dba0a76a6f5f6c0');
